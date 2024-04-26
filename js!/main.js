b837efaf0612d6c45459667ecd2dc0bd180d82d5
gaji12345 <gajimax12345@gmail.com>
2024-04-26 11:43:55 +0530

    hide photos use js

--- js!/main.js
+++ js!/main.js
@@ -13,10 +13,38 @@ document.querySelector('img').getAttribute("src")
 
 document.querySelector("#btn1").addEventListener('click',function () {
 
-    document.querySelector("img").setAttribute("src",  document.querySelector("input").value);
+   document.querySelector("img").setAttribute("src",  document.querySelector("input").value);
+    //document.getElementById("#im1").style.display='none';
 })
 
 document.querySelector("#bb").addEventListener('keyup',function () {
 
     document.querySelector("#main").innerText=  document.querySelector("#bb").value;
 })
+
+//photo hide
+
+document.getElementById("img1").addEventListener('click',function () {
+
+    document.getElementById("im1").style.display='none';
+    document.getElementById("im2").style.display='inline-block';
+    document.getElementById("im3").style.display='inline-block';
+
+});
+
+document.getElementById("img2").addEventListener('click',function () {
+
+    document.getElementById("im1").style.display='inline-block';
+    document.getElementById("im2").style.display='none';
+    document.getElementById("im3").style.display='inline-block';
+
+});
+document.getElementById("img3").addEventListener('click',function () {
+
+    document.getElementById("im1").style.display='inline-block';
+    document.getElementById("im2").style.display='inline-block';
+    document.getElementById("im3").style.display='none';
+
+});
+
+
