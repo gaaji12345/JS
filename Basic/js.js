0f6c1cbd2ca6e9160646a921640b4522de22aab5
gaji12345 <gajimax12345@gmail.com>
2024-04-30 12:19:29 +0530

    test array and ob

---
+++ Basic/js.js
@@ -0,0 +1,48 @@
+//-litral based arrays
+
+var myName=['gaaji','kasun','chitha'];
+
+myName[2];
+
+//function based
+
+var ages=new Array(10,20,30);
+
+
+//loops
+
+// for (var i=0;i<10;i++){
+//     console.log(i+1);
+// }
+
+// var i=0;
+// while (i<10){
+//     i++;
+//     console.log(i)
+// }
+
+// var i=0;
+// do {
+//
+//     console.log(i+1);
+//     i++;
+// }while (i<10);
+
+var array=[10,20,30,40,50];
+
+// for (let i = 0; i < array.length; i++) {
+//      console.log(array[i]);
+// }
+
+///for of ->array values to the variable
+// for (var i of array){
+//     console.log([i]);
+// }
+
+//for in -> array eke index numbers
+for (var i in array){
+    console.log([i]+",");
+}
+
+
+
