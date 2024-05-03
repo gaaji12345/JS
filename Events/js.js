0a205227d2c958a259e3c10fe04e8726fde1bf22
gaji12345 <gajimax12345@gmail.com>
2024-05-03 08:58:43 +0530

    keypress,keydown,keyup ->check

--- Events/js.js
+++ Events/js.js
@@ -3,10 +3,44 @@
 // })
 
 $('#btnSaveustomer').click(function (){
-    alert('gaaji');
+   // alert('gaaji');
 })
 
 $('#tb>tr').on('dblclick',function (){
     $(this).remove();
 });
 
+$('#txtCid').on('focus',function (){
+    //alert('gaaji');
+    console.log("gaaji")
+});
+
+
+$('#txtcName').on('blur',function (){
+    //alert('gaaji');
+    console.log("gaajizzz")
+});
+
+$('#txtAddress').on('keydown',function (event){
+    //alert('gaaji');
+    console.log(event)
+});
+$('#txtSalry').on('keydown',function (){
+    //alert('gaaji');
+    console.log("down")
+});
+
+$('#txtSalry').on('keyup',function (){
+    //alert('gaaji');
+    console.log("up")
+});
+$('#txtSalry').on('keypress',function (){
+    //alert('gaaji');
+    console.log("press")
+});
+
+
+$('#txtAddress').on('keydown',function (event){
+    //alert('gaaji');
+    $('#hhh').text( event.key);
+});
