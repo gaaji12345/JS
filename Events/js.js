06f17beb68c344a9e120c1c1b64760ee206d569b
gaji12345 <gajimax12345@gmail.com>
2024-05-03 08:15:42 +0530

    test events

---
+++ Events/js.js
@@ -0,0 +1,12 @@
+// $('#hh').onmousedown(function (){
+//     alert('gaaji');
+// })
+
+$('#btnSaveustomer').click(function (){
+    alert('gaaji');
+})
+
+$('#tb>tr').on('dblclick',function (){
+    $(this).remove();
+});
+
